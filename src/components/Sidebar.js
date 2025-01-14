1a78148e686475ea03f33add34f0895495ae73a9
Priyush02K <collegeinfopk02@gmail.com>
2025-01-14 13:42:52 +0530

    part1-done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 // import store from '../utils/store'
+import { Link } from "react-router-dom";
+
 
 const Sidebar = () => {
   const isMenuOpen =useSelector((store) => store.app.isMenuOpen);
@@ -11,14 +13,15 @@ const Sidebar = () => {
   return (
     <div className="p-5 shadow-lg  w-48 ">
       <ul>
-        <li>Home</li>
+        <li><Link  to="/">Home</Link></li>
         <li>Short</li>
         <li>Videos</li>
         <li>Live</li>
+    
 
       </ul>
 
-      <h1 className="font-bold"> Subscription</h1>
+      <h1 className="font-bold pt-5"> Subscription</h1>
           <ul>
             <li>Music</li>
             <li>Sport</li>
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from "react-redux";
+import { closeMenu } from '../utils/appSlice';
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const dispatch=useDispatch();
+  useEffect (()=>{
+    dispatch(closeMenu())
+  },[])
+  
+  
+  
   return (
-    <div>
-      wacpage
+    <div className="flex flex-col w-full">
+      <div className="px-5 flex w-full">
+        <div className="">
+          <iframe
+            width="1000"
+            height="500"
+            src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+            
+          ></iframe>
+        </div>
+        {/* <div className="w-full">
+          <LiveChat />
+        </div> */}
+      </div>
+      {/* <CommentsContainer /> */}
     </div>
-  )
-}
+  );
+
+};
 
 export default WatchPage;
